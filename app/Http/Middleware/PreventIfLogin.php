f182d3a5577a9971bbc41ce5bdf24dfa82cc3e46
dervistprk <dervistprk@gmail.com>
2022-08-01 19:05:50 +0300

    - Various style related changes.

---
+++ app/Http/Middleware/PreventIfLogin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class PreventIfLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (\Auth::check()) {
+            return redirect()->route('home')->with('message', 'Yeni bir üyelik almak için lütfen öncelikle mevcut üyeliğinizden çıkış yapınız.');
+        }
+        return $next($request);
+    }
+}
